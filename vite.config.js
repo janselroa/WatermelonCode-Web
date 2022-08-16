27700b4998b496a19a5bd30912836d8da1ba1537
Jansel <janselroa2424@gmail.com>
2022-08-16 14:42:03 -0400

    add minified and optimization

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
-export default {
+import { defineConfig } from 'vite'
+import handlebars from 'vite-plugin-handlebars';
+import { resolve } from 'path';
+import { ViteMinifyPlugin } from 'vite-plugin-html'
+
+export default defineConfig({
     base: './',
-}  
+    plugins: [
+      ViteMinifyPlugin({}),
+      handlebars({
+        partialDirectory: resolve(__dirname, 'src/partials'),
+        })
+    ],
+  })
