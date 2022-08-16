39667e5c83161b9cf4e03c58c3593b9b6dbe8ac6
= <=>
2022-08-15 19:45:01 -0500

    assets deleted and rechanged

--- assets/js/countEfect.js
+++
@@ -1,9 +0,0 @@
-const counter = document.querySelector(".miembros")
-let contador=1000
-const interval = setInterval(()=>{
-    contador++
-    counter.textContent=contador
-    if(contador>1250){
-        clearInterval(interval)
-    }
-},0)
