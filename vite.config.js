cd1c0536a542317a503fe64a9b340c475d109825
Jansel Roa <65916518+janselroa@users.noreply.github.com>
2022-08-17 00:53:43 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars';
 import { resolve } from 'path';
-import { ViteMinifyPlugin } from 'vite-plugin-html'
-
 export default defineConfig({
     base: './',
     plugins: [
-      ViteMinifyPlugin({}),
       handlebars({
         partialDirectory: resolve(__dirname, 'src/partials'),
         })
