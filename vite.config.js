ee0a7174d84801adc7cfaf87f3a3d46d6eb7dc83
mrLuisFer <mrluisfeer@gmail.com>
2022-08-17 21:50:00 -0500

    fix unclose article tag

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,9 @@ import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars';
 import { resolve } from 'path';
 import { createHtmlPlugin } from 'vite-plugin-html'
-
 export default defineConfig({
   base: './',
   plugins: [
-    // ViteMinifyPlugin({}),
-    createHtmlPlugin({
-      minify: true,
-      entry: 'src/main.ts',
-      template: 'index.html'
-    }),
     handlebars({
       partialDirectory: resolve(__dirname, 'src/partials'),
     })
