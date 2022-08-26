1507dd79512ffe8978fde69611444979f025b346
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-08-26 18:21:54 -0500

    path resolved

--- src/ts/changeImage.ts
+++ src/ts/changeImage.ts
@@ -1,11 +1,4 @@
-// TODO: change types
-
-// TODO: compress the images (are so big)
-if(import.meta.env.PROD) var root = "./assets"
-else var root = "./src/assets"
 export const changeImage = (element: any, images: string[]): void => {
-  console.log(element)
-
   let i = 0
   setInterval(() => {
     element.classList.add('animation')
@@ -13,7 +6,7 @@ export const changeImage = (element: any, images: string[]): void => {
       element.classList.remove('animation')
     }, 500)
     if (i >= images.length) i = 0
-    element.src = `${root}/img/${images[i]}`
+    element.src = `/${images[i]}`
     i += 1
   }, 5000)
 }
