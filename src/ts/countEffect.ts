586c432c00f8428ff5e35390b89d75435d9cae5e
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-08-26 18:22:07 -0500

    ignore this change

--- src/ts/countEffect.ts
+++ src/ts/countEffect.ts
@@ -1,21 +1,22 @@
 export function countEffect(): void {
   const elements = [{
-    selector:".miembros",
-    maxNumber:1260,
-    initialNumber:1000
+    selector: '.miembros',
+    maxNumber: 1260,
+    initialNumber: 1000,
   },
-  {
-    selector:".helpers",
-    maxNumber:20,
-    initialNumber:1
-  }
-  ,{
-    selector:".eventos",
-    maxNumber:25,
-    initialNumber:2
-  }
+    {
+      selector: '.helpers',
+      maxNumber: 20,
+      initialNumber: 1,
+    }
+    , {
+      selector: '.eventos',
+      maxNumber: 25,
+      initialNumber: 2,
+    },
   ]
-  for(let element of elements){
+
+  for (let element of elements) {
     const counter: HTMLElement = document.querySelector<HTMLElement>(element.selector) as HTMLElement
     let contador = element.initialNumber
     const interval = setInterval(() => {
